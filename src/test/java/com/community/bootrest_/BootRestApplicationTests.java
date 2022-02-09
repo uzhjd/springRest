042b248e7bf164f3ab68b6f1c9ee54c201c2edab
uzhjd <73466440+uzhjd@users.noreply.github.com>
2022-02-09 22:36:25 +0900

    first commit

---
+++ src/test/java/com/community/bootrest_/BootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.bootrest_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
